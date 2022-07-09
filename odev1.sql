3c78e37bdf7ef1c72ab0f121ee49884f7830d4e6
zynpkrsy <zynpkrsy1@gmail.com>
2022-07-09 07:13:12 +0300

    Update odev1.sql

--- odev1.sql
+++ odev1.sql
@@ -13,6 +13,6 @@ WHERE rental_rate = 0.99 AND (replacement_cost = 12.99 OR replacement_cost = 28.
 SELECT last_name FROM customer
 WHERE first_name = 'Mary';
 
-film tablosundaki uzunluğu(length) 50 ten büyük OLMAYIP aynı zamanda rental_rate değeri 2.99 veya 4.99 OLMAYAN verileri sıralayınız.
+-- film tablosundaki uzunluğu(length) 50 ten büyük OLMAYIP aynı zamanda rental_rate değeri 2.99 veya 4.99 OLMAYAN verileri sıralayınız.
 SELECT * FROM film
 WHERE NOT length > 50 AND NOT (rental_rate = 2.99 OR rental_rate = 4.99);
